4131e66ee665748d1d1ec85eaec1de08d1449604
Thiti Dev <thiti.mwk.main@gmail.com>
2020-10-17 23:34:50 +0700

    added disclamer and also loading indicator&its condition

--- src/components/pages/main/Main.vue
+++ src/components/pages/main/Main.vue
@@ -1,18 +1,35 @@
 <template>
-    <div class="container">
-        <h1>Frontend Framework Popularity 2020-2021</h1>
-    </div>
+    <v-container>
+        <v-row justify="center">
+            <h1>Frontend Framework Popularity 2020-2021</h1>
+            <Disclaimer @onAccept="proceedThroughTheDisclamation" :isViewing="isViewing"/>        
+        </v-row>
+        <div v-if="isLoading && isViewing" class="text-center mt-15">
+            <v-progress-circular
+                :size="50"
+                color="primary"
+                indeterminate
+            ></v-progress-circular>
+        </div>
+    </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-@Component
+import Disclaimer from './wrapper/Disclaimer.vue'
+@Component({
+    components: {Disclaimer}
+})
 export default class Main extends Vue{
-
+    private isViewing = false;
+    private isLoading = false;
+    
+    public proceedThroughTheDisclamation(): void{
+        this.isViewing = true
+        this.isLoading = true
+    }
 }
 </script>
 <style lang="scss" scoped>
-.container{
-    text-align: center;
-}
+
 </style>
---
+++ src/components/pages/main/wrapper/Disclaimer.vue
@@ -0,0 +1,43 @@
+<template>
+    <div v-if="!isViewing">
+        <v-alert
+            class="disclaimer"
+            outlined
+            type="warning"
+            prominent
+            border="left"
+        >
+            Disclaimer: This website's using the statistic data from the npm.js website directly (visit them instead if you want)
+        </v-alert>
+        <v-btn
+            @click="proceed()"
+            class="proceed_btn"
+            depressed
+            color="primary"
+            >
+            Proceed
+        </v-btn>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Emit, Prop, Vue } from 'vue-property-decorator'
+@Component
+export default class Disclaimer extends Vue{
+    @Prop() isViewing!: boolean
+
+    @Emit('onAccept')
+    proceed(){
+        return true
+    }
+
+}
+</script>
+<style lang="scss" scoped>
+.disclaimer{
+    margin-top: 100px;
+}
+.proceed_btn{
+    float: right;
+}
+</style>
