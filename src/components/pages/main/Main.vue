8ffd8c0c8ff344ee5c592114124017a1cb8d73a9
Thiti Dev <thiti.mwk.main@gmail.com>
2020-10-17 22:33:56 +0700

    :fire: Removed vue auto-generated boring class with log and replacing with the custom written one

---
+++ src/components/pages/main/Main.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <h1>Frontend Framework Popularity 2020-2021</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component
+export default class Main extends Vue{
+
+}
+</script>
+<style lang="scss" scoped>
+.container{
+    text-align: center;
+}
+</style>
