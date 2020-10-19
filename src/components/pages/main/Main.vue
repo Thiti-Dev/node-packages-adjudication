e2fa14f7dcc27a2f025e705dccd2bdbcd639f56e
Thiti Dev <thiti.mwk.main@gmail.com>
2020-10-19 21:53:33 +0700

    :zap: Finalized renderer condition for main (Npc)views

--- src/components/pages/main/Main.vue
+++ src/components/pages/main/Main.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container>
-        <v-row justify="center">
-            <h1>Frontend Framework Popularity 2020-2021</h1>
+        <v-row v-if="!isViewing" justify="center">
+            <h2>Node packages comparison</h2>
             <Disclaimer @onAccept="proceedThroughTheDisclamation" :isViewing="isViewing"/>        
         </v-row>
         <div v-if="isLoading && isViewing" class="text-center mt-15">
@@ -11,14 +11,16 @@
                 indeterminate
             ></v-progress-circular>
         </div>
+        <Npc v-if="isViewing && !isLoading"/>
     </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import Disclaimer from './wrapper/Disclaimer.vue'
+import Npc from './views/npc/Npc.vue'
 @Component({
-    components: {Disclaimer}
+    components: {Disclaimer,Npc}
 })
 export default class Main extends Vue{
     private isViewing = false;
@@ -27,6 +29,9 @@ export default class Main extends Vue{
     public proceedThroughTheDisclamation(): void{
         this.isViewing = true
         this.isLoading = true
+        setTimeout(() => {
+                this.isLoading = false
+        }, 1000);
     }
 }
 </script>
